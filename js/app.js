a0a3e6a1e50fa3d25839e37eb51aa3beee531ae7
Mazen El Ghazzawi <90219094+Metallmad@users.noreply.github.com>
2023-04-22 04:57:37 +0300

    change_22_4-2

--- js/app.js
+++ js/app.js
@@ -27,16 +27,17 @@ const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header')
 
 function toggleSkills(){
-  let itemClass = this.parentNode.ClassName
+  let itemClass = this.parentNode.className
 
-  for (i = 0; i < skillsContent.length; i++) {
+  for(i = 0; i < skillsContent.length; i++){
     skillsContent[i].className = 'skills__content skills__close'
   }
-  if (itemClass === 'skills__content skills__close') {
-    this.parentNode.className = 'skills__content skills__open'
+  if(itemClass === 'skills__content skills__close'){
+      this.parentNode.className = 'skills__content skills__open'
   }
 }
 
-skillsHeader.forEach((el) => {
+skillsHeader.forEach((el) =>{
   el.addEventListener('click', toggleSkills)
 })
+
