5d10e8d08b8208234fefa4d4eec2533b07e9529d
Metallmad <90219094+Metallmad@users.noreply.github.com>
2023-04-23 23:50:12 +0300

    changes_23_4-2nd

--- js/app.js
+++ js/app.js
@@ -43,22 +43,23 @@ skillsHeader.forEach((el) =>{
 
 //qualification tab
 
-const tabs = document.querySelectorAll('[data-target]'),
-      tabContent = document.querySelectorAll('[data-content]');
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target)
+// const tabs = document.querySelectorAll('[data-target]'),
+//       tabContent = document.querySelectorAll('[data-content]');
 
-    tabContent.forEach(tabContent => {
-      tabContent.classList.remove('qualification__active')
-    })
-    target.classList.add('qualification__active')
+// tabs.forEach(tab => {
+//   tab.addEventListener('click', () => {
+//     const target = document.querySelector(tab.dataset.target)
 
-    tab.forEach(tab =>{
-      tab.classList.remove('qualification__active')
-    })
-    tab.classList.add('qualification__active')
-  })
-})      
+//     tabContent.forEach(tabContent => {
+//       tabContent.classList.remove('qualification__active')
+//     })
+//     target.classList.add('qualification__active')
+
+//     tab.forEach(tab =>{
+//       tab.classList.remove('qualification__active')
+//     })
+//     tab.classList.add('qualification__active')
+//   })
+// })      
 
