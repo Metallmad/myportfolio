e528b7c4776a510981ac6f4abdd38eec0e859cf5
Mazen El Ghazzawi <90219094+Metallmad@users.noreply.github.com>
2023-04-23 21:43:06 +0300

    change_23-4-1st

--- js/app.js
+++ js/app.js
@@ -41,3 +41,24 @@ skillsHeader.forEach((el) =>{
   el.addEventListener('click', toggleSkills)
 })
 
+//qualification tab
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContent.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tab.forEach(tab =>{
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})      
+
