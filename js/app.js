02ba0e6b4c4ed1e1920f73f0a5b0d3525d55586a
Mazen El Ghazzawi <90219094+Metallmad@users.noreply.github.com>
2023-09-22 21:38:33 +0300

    22-9-1

--- js/app.js
+++ js/app.js
@@ -122,3 +122,41 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   }
 });
 
+//scroll sections
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+//change header background
+
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  if(this.scrollY >= 80) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+//show scroll
+
+function scrollUp(){
+  const scrollUp = documnet.getElementById('scroll-up');
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
